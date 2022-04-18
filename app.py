e0683c6a8e92923402a06a4367285a086a210e2e
Rahul Sharma <37498925+technorsh@users.noreply.github.com>
2022-04-18 17:08:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 #   chmod +x scripts.sh
 #   python3 ssm_json_to_csv.py <JSON File name> <CSV File name>
 
-import pandas as pd
 import sys
 import subprocess
 subprocess.call(["./scripts.sh",sys.argv[1]])
+import pandas as pd
 df = pd.read_json("ssm.json") # temporary file > ssm.json
 df = df["Parameters"]
 a = map(lambda x: x,df)
